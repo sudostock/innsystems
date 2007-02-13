4d5183f0338a48a4b93c1dd6278dcf3c2d518312
darkbird44 <darkbird44@b502e547-4a29-0410-9e77-ff969709daea>
2007-02-13 17:54:40 +0000

    Added netComm class

---
+++ innsystems/src/networking/netComm.java
@@ -0,0 +1,28 @@
+/*
+ * netComm.java
+ *
+ * Created on February 13, 2007, 11:13 AM
+ *
+ */
+
+package networking;
+
+import java.net.*;
+
+
+/**
+ * Responsible for communication between server and client. Handles all of the sending and recieving of data
+ *
+ *
+ * @author Alex Filby
+ * @version 0.0.1 2/13/06
+ */
+public class netComm {
+    
+    /** Creates a new instance of netComm */
+    public netComm() {
+    }
+    
+   
+    
+}
